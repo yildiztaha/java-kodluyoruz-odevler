15e854d51febcc0562768fa9806149edf4461910
yildiztaha <tahayildiz2001@gmail.com>
2023-09-15 15:09:49 +0300

    manav projesi eklendi

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double para,kdvTutarı,toplamFiyat;
+        int kg;
+//        armut: 2,14 TL , Elma : 3,67 TL , Domates : 1,11 TL , Muz: 0,95 TL , Patlıcan : 5,00 TL
 
+        System.out.print("Armut Kaç Kilogram: ");
+        kg = input.nextInt();
+        float armutKg = (float) (kg * 2.14F);
 
-        System.out.print("Fiyat Giriniz: ");
-        para = input.nextDouble();
+        System.out.print("Elma Kaç Kilogram: ");
+        kg = input.nextInt();
+        float elmaKg = (float) (kg * 3.67F);
 
-        if ( 0 < para && para < 1000){
-            kdvTutarı = (para/100)*18 ;
-            toplamFiyat = kdvTutarı + para;
-            System.out.println("KDV'siz Fiyat: "+para);
-            System.out.println("KDV'li Fiyat: "+toplamFiyat);
-            System.out.println("KDV Tutarı: "+kdvTutarı);
-        }else if (para > 1000) {
-            kdvTutarı = (para/100)*8 ;
-            toplamFiyat = kdvTutarı + para;
-            System.out.println("KDV'siz Fiyat: "+para);
-            System.out.println("KDV'li Fiyat: "+toplamFiyat);
-            System.out.println("KDV Tutarı: "+kdvTutarı);
-        }
+        System.out.print("Domates Kaç Kilogram: ");
+        kg = input.nextInt();
+        float domatesKg = (float) (kg * 1.11F);
+
+        System.out.print("Muz Kaç Kilogram: ");
+        kg = input.nextInt();
+        float muzKg = (float) (kg * 0.95F);
+
+        System.out.print("Patlıcan Kaç Kilogram: ");
+        kg = input.nextInt();
+        float patlicanKg = (float) (kg * 5.00F);
+
+        float toplamTutar = (armutKg + elmaKg + domatesKg + muzKg + patlicanKg);
+        System.out.print("Toplam tutar: "+toplamTutar);
     }
 }
