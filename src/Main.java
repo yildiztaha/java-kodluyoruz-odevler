764b1d21238cd01e2a3a4ed52c9505768c088b2e
yildiztaha <tahayildiz2001@gmail.com>
2023-09-19 11:55:00 +0300

    pratik-toplama

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number, summer = 0;
+
+/*        do {
+            System.out.print("Sayi Giriniz: ");
+            number = scan.nextInt();
+            if (number % 2 == 1) {
+                summer += number;
+            }
+        } while (number > 0);
+
+        System.out.println("Toplam: "+summer);*/
+
+        do {
+            System.out.print("Sayi Giriniz: ");
+            number = scan.nextInt();
+            if ((number % 2 == 0) && (number % 4 == 0)) {
+                summer += number;
+            }
+        } while (!(number % 2 == 1));
+
+        System.out.println("Toplam: "+summer);
+
+    }
+
+}
